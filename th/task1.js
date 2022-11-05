99ff0fb0e1f0727443c1a36a9df8c57d6c76f988
israa muhaisen <israa.muhaisen89@gmail.com>
2022-11-05 18:47:00 +0200

    project

--- th/task1.js
+++ th/task1.js
@@ -2,8 +2,8 @@ var x=Number(prompt("Enter the first  value"));
 var y=Number((prompt("Enter the second value")));
 console.log(x,y)
 if (x>y) {
-    document.getElementById(`output`).innerText = 'Hello World';    
+document.write (`Hello World`);
 }
 else{
-    document.getElementById(`output`).innerText = 'Goodbye';
+   alert("GoodBye");
 }
