5611a80b358172216b9fa7170aed9558c4e28527
israa muhaisen <israa.muhaisen89@gmail.com>
2022-11-08 00:09:29 +0200

    Change

---
+++ monday/task10.js
@@ -0,0 +1,14 @@
+var x=prompt("insert first string");
+var y=prompt("insert second string");
+
+function sameLength(x,y){
+    if(x.length == y.length){
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+}
+sameLength(x,y);
+
+
